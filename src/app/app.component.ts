4bd9186894fbbd050d8cbbcd2f5711192e221e68
Yossi Ben Zaken <yossibz@gmail.com>
2022-11-25 23:53:45 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, NavbarComponent],
 })
 export class AppComponent {
   title = 'eva-inwise';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,84 +1,74 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'templates',
+    pathMatch: 'full',
+  },
+  {
+    path: 'templates',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'files',
+    loadComponent: () =>
+      import('./show-files/show-files.component').then(
+        (m) => m.ShowFilesComponent
+      ),
+  },
+  {
+    path: 'tags',
+    loadComponent: () =>
+      import('./tags/tags.component').then((m) => m.TagsComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'smsReports',
+    loadComponent: () =>
+      import('./sms-report/sms-report.component').then(
+        (m) => m.SmsReportComponent
+      ),
+  },
+  {
+    path: 'unsubscribe',
+    loadComponent: () =>
+      import('./unsubscribe/unsubscribe.component').then(
+        (m) => m.UnsubscribeComponent
+      ),
+  },
+  {
+    path: 'settings',
+    loadComponent: () =>
+      import('./settings/settings.component').then((m) => m.SettingsComponent),
+  },
+  {
+    path: 'QA',
+    loadComponent: () => import('./qa/qa.component').then((m) => m.QaComponent),
+  },
+  {
+    path: 'DA4',
+    loadComponent: () =>
+      import('./distribution-at-four/distribution-at-four.component').then(
+        (m) => m.DistributionAtFourComponent
+      ),
+  },
+];
+
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    RouterModule.forRoot(
-      [
-        {
-          path: '',
-          redirectTo: 'templates',
-          pathMatch: 'full',
-        },
-        {
-          path: 'templates',
-          loadComponent: () =>
-            import('./home/home.component').then((m) => m.HomeComponent),
-        },
-        {
-          path: 'files',
-          loadComponent: () =>
-            import('./show-files/show-files.component').then(
-              (m) => m.ShowFilesComponent
-            ),
-        },
-        {
-          path: 'tags',
-          loadComponent: () =>
-            import('./tags/tags.component').then((m) => m.TagsComponent),
-        },
-        {
-          path: 'login',
-          loadComponent: () =>
-            import('./login/login.component').then((m) => m.LoginComponent),
-        },
-        {
-          path: 'smsReports',
-          loadComponent: () =>
-            import('./sms-report/sms-report.component').then(
-              (m) => m.SmsReportComponent
-            ),
-        },
-        {
-          path: 'unsubscribe',
-          loadComponent: () =>
-            import('./unsubscribe/unsubscribe.component').then(
-              (m) => m.UnsubscribeComponent
-            ),
-        },
-        {
-          path: 'settings',
-          loadChildren: () =>
-            import('./settings/settings.component').then(
-              (m) => m.SettingsComponent
-            ),
-        },
-        {
-          path: 'QA',
-          loadComponent: () =>
-            import('./qa/qa.component').then((m) => m.QaComponent),
-        },
-        {
-          path: 'DA4',
-          loadComponent: () =>
-            import(
-              './distribution-at-four/distribution-at-four.component'
-            ).then((m) => m.DistributionAtFourComponent),
-        },
-      ],
-      {
-        useHash: true,
-      }
-    ),
-  ],
+  declarations: [],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [],
 })
 export class AppModule {}
